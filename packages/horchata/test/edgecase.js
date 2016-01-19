d072ca034c9b6a068e4be4c5fea1c19694525d05
Jordan Klassen <jordan@klassen.me.uk>
2016-01-18 16:23:30 -0800

    tmp

---
+++ packages/horchata/test/edgecase.js
@@ -0,0 +1,38 @@
+/*global suite,test*/
+require('source-map-support').install()
+
+var fs = require("fs")
+var path = require("path")
+
+var _ = require("lodash")
+var chai = require("chai")
+var devUtils = require("../../tacoscript-dev-utils")
+chai.use(devUtils.chaiHelper)
+var expect = chai.expect;
+var render = require("tacoscript-cst-utils").render
+
+var horchata = require("../lib/index")
+
+// TODO: rewrite mocha-fixtures-generic to be more generic, w.r.t directory structure
+
+var fixtureRootDir = path.join(__dirname, "fixtures/edgecase")
+var fixtureDirs = fs.readdirSync(fixtureRootDir)
+
+suite("horchata", function () {
+  suite("edgecase", function () {
+    _.forEach(fixtureDirs, function(fixtureDir) {
+      test(fixtureDir, function () {
+        var fixtureAstPath = path.join(fixtureRootDir, fixtureDir, "ast.json")
+        var fixtureAst; try { fixtureAst = require(fixtureAstPath) } catch(e) {}
+
+        var source = fs.readFileSync(path.join(fixtureRootDir, fixtureDir, "source.taco"), "utf-8")
+        var ast = horchata.parse(source)
+        if (fixtureAst) {
+          expect(ast).matches(fixtureAst)
+        } else {
+          saveAst(fixtureAstPath, ast)
+        }
+      })
+    })
+  })
+})
