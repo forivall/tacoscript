3867849b25d9921a82c509202feb9fa39c6d6583
Jordan Klassen <jordan@klassen.me.uk>
2016-01-19 13:26:38 -0800

    Update dev-utils

--- packages/tacoscript-dev-utils/index.js
+++ packages/tacoscript-dev-utils/index.js
@@ -1,15 +1,18 @@
 // borrowed from babel/packages/babylon/test.js
 // TODO: move to external library
 
+var fs = require('fs')
+
 function ppJSON(v) {
   return v instanceof RegExp ? v.toString() : JSON.stringify(v, null, 2);
 }
 
 function addPath(str, pt) {
   if (str.charAt(str.length - 1) == ")") {
-    return str.slice(0, str.length - 1) + "/" + pt + ")";
+    var idx = str.lastIndexOf("(.");
+    return str.slice(0, idx + 2)  + pt + "." + str.slice(idx + 2, str.length - 1) + ")";
   } else {
-    return str + " (" + pt + ")";
+    return str + " (." + pt + ")";
   }
 }
 
@@ -36,6 +39,63 @@ function misMatch(exp, act) {
   }
 }
 
+function chaiHelper(chai, utils) {
+  var Assertion = chai.Assertion
+  Assertion.addMethod('matches', function(ast) {
+    var obj = this._obj
+    var mismatched = misMatch(ast, obj)
+    this.assert(!mismatched, mismatched, "expected ast to not be equal", ast, obj)
+  })
+}
+
+
+function removeLocInfo(json) {
+  if (Object.prototype.toString.call(json) === '[object Array]') {
+    // for (var i = 0, len = json.length; i < len; i++) {
+    // walk backwards so that duplicate trailingComments are removed
+    for (var i = json.length - 1; i >= 0; i--) {
+      if (json[i] != null) removeLocInfo(json[i]);
+    }
+  } else {
+    delete json.start;
+    delete json.end;
+    delete json.loc;
+    delete json.tokenStart;
+    delete json.tokenEnd;
+    delete json.sourceElements;
+    delete json.sourceElementsTACO;
+    if (json.leadingComments == null) delete json.leadingComments;
+    if (json.innerComments == null) delete json.innerComments;
+    if (json.trailingComments == null) delete json.trailingComments;
+    if (json.type === "BlockStatement" && json.directives != null && json.directives.length === 0) {
+      delete json.directives;
+    }
+    for (var k in json) {
+      if (json[k] != null && (typeof json[k]) === 'object') {
+        removeLocInfo(json[k]);
+        if (k === "extra") {
+          delete json.extra.rawValue;
+          delete json.extra.parenStart;
+          if (Object.keys(json.extra).length === 0) delete json.extra;
+        }
+      }
+    }
+  }
+  // TODO: store this modified JSON
+  return json;
+}
+
+function saveAst(filepath, ast) {
+  delete ast.tokens
+  delete ast.source
+  devUtils.removeLocInfo(ast)
+  fs.writeFileSync(filepath, JSON.stringify(ast, null, '  '), 'utf-8')
+  throw new Error("Unverified ast file: " + fixtureAstPath)
+}
+
 exports.ppJSON = ppJSON;
 exports.addPath = addPath;
 exports.misMatch = misMatch;
+exports.chaiHelper = chaiHelper;
+exports.removeLocInfo = removeLocInfo;
+exports.saveAst = saveAst;
