934616aba1f702676bf38fd7b5f06998443b4ce0
Jordan Klassen <jordan@klassen.me.uk>
2016-02-02 15:23:08 -0800

    wip predetection of indent

---
+++ packages/horchata/src/lexer/detect-indent.js
@@ -0,0 +1,60 @@
+import repeating from 'repeating';
+import {types as ct} from './context-types';
+import Lexer from './index';
+
+import {lineBreak, lineBreakG, isNewline, nonASCIIwhitespace} from "./whitespace"
+
+// eventually, the tokenizer should be modular enough that we will be able to
+// reuse it to run this preprocessing pass
+
+export default function (input) {
+  if (typeof input !== 'string') {
+    throw new TypeError('Expected a string');
+  }
+
+  const len = input.length;
+  let pos = 0, ch, indentCh = -1, indentLen;
+  let amount = Infinity;
+  let inIndent = true;
+  let curContext = ct.stmt;
+  let context = [];
+
+  top:
+  while (pos < len) {
+    ch = input.charCodeAt(pos);
+    switch (curContext) {
+      case ct.stmt:
+      case ct.obj_expr:
+      // case ct.
+    }
+    if (indentCh === -1) {
+      // whitespace
+      if (ch === 32 || ch === 160 || ch === 9 || ch === 11 || ch === 12 ||
+          ch >= 5760 && nonASCIIwhitespace.test(String.fromCharCode(ch))) {
+        indentCh = ch;
+      }
+    }
+
+
+
+  // lineBreak.lastIndex = pos, match = lineBreak.exec(str)))
+  }
+
+  if (amount > 9999) amount = 0;
+  return {
+    amount: amount,
+    type: indentCh === 9 ? 'tab' : 'space',
+    indent: repeating(amount, String.fromCharCode(indentCh)),
+    irregular: indentCh !== 9 && indentCh !== 32,
+    nonAscii: indentCh >= 5760
+  }
+}
+
+function nextValidLineStart(input, pos) {
+  // TODO
+  // skip comments
+  // skip lines with just comments
+  // skip code wrapped in parens, except functions and statementy expressions
+  // skip empty lines
+  // stop on a newline with leading whitespace
+}
