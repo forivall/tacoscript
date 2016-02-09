7fb65bd42e257ec329fd11e17b101542e891c288
Jordan Klassen <jordan@klassen.me.uk>
2016-02-09 15:24:07 -0800

    Add babel-helper-transform-fixture-test-runner to fork list

--- scripts/manage-forks.sh
+++ scripts/manage-forks.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+# NOTE: this script shouldn't actually run with --init, it's more for documentation
+
 if ! git --git-dir ../babel/.git rev-parse 2>/dev/null ; then
   (cd ..
     git clone git://github.com/babel/babel.git
@@ -12,11 +14,14 @@ fi
   git pull
   git subtree split -P packages/babel-traverse master -b subtree_babel-traverse
   git subtree split -P packages/babel-types master -b subtree_babel-types
+  git subtree split -P packages/babel-helper-transform-fixture-test-runner master -b subtree_babel-helper-transform-fixture-test-runner
 )
 
 if [ "$1" = "--init" ] ; then
+  git subtree add -P packages/comal ../babel subtree_babel-core --squash
   git subtree add -P packages/comal-traverse ../babel subtree_babel-traverse --squash
   git subtree add -P packages/comal-types ../babel subtree_babel-types --squash
+  git subtree add -P packages/tacoscript-dev-utils/fixture-runner ../babel subtree_babel-helper-transform-fixture-test-runner --squash
 else
   git subtree pull -P packages/comal-traverse ../babel subtree_babel-traverse --squash
   git subtree pull -P packages/comal-types ../babel subtree_babel-types --squash
