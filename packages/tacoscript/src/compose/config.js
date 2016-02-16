3db87278c168f8fd77c8ed688baf16d5039a8618
Jordan Klassen <jordan@klassen.me.uk>
2016-02-16 13:02:52 -0800

    fix(tacoscript/cli): allow passing generator options in directly

--- packages/tacoscript/src/compose/config.js
+++ packages/tacoscript/src/compose/config.js
@@ -3,4 +3,8 @@ export default {
     description: "Declares whether the code should be parsed as a script or a module",
     default: "module"
   },
+
+  generate: {
+    description: "arguments to pass into the generator (babel-generator by default)"
+  }
 };
