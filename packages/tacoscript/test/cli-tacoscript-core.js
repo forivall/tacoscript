abd112b13888dd3ad8d2b3f86358647e22d55136
Jordan Klassen <jordan@klassen.me.uk>
2016-02-17 17:28:02 -0800

    test(tacoscript/cli): add basic tests for the cli

---
+++ packages/tacoscript/test/cli-tacoscript-core.js
@@ -0,0 +1,57 @@
+require('source-map-support').install();
+
+var spawn = require("child_process").spawn;
+var concat = require("concat-stream");
+var path = require("path");
+var sourceMap = require("source-map");
+
+var assert = require("chai").assert;
+
+var cli = require("../lib/cli/tacoscript");
+
+function testRun(args, exitCode) {
+  if (exitCode == null) exitCode = 0;
+  return function(done) {
+    process.chdir(__dirname);
+
+    var ps = spawn(process.execPath, [
+      path.resolve(__dirname, '../bin/tacoscript.js'),
+    ].concat(args));
+
+    var hasOutput = false;
+    ps.stdout.pipe(concat(function (body) {
+      if (body) hasOutput = true;
+    }));
+    ps.stderr.pipe(process.stderr);
+
+    ps.on('exit', function (code) {
+      assert.ok(hasOutput);
+      assert.equal(code, exitCode);
+      done();
+    });
+
+    ps.stdin.end();
+  }
+}
+
+suite("cli", function () {
+
+  setup(function () {
+    this.cwd = process.cwd();
+  });
+
+  teardown(function () {
+    process.chdir(this.cwd);
+  });
+
+  test("help", testRun(['-h']));
+  test("help compose", testRun(['-h', 'compose']));
+  test("help compose 2", testRun(['compose', '-h']));
+  test("help compose advanced", testRun(['compose', '-h', 'advanced']));
+  test("version", testRun(['-V']));
+  test("versions", testRun(['--VV']));
+  test("versions 2", testRun(['version']));
+  test("unknown command", testRun(['_blah'], 1));
+  test("unknown command 2", testRun(['blah'], 1));
+  test("unknown command 3", testRun(['index'], 1));
+});
