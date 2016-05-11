8f5a5ffcd340d17120938c4a1f5325b37f097498
Jordan Klassen <jordan@klassen.me.uk>
2016-05-10 22:15:20 -0700

    fix: fixup node command from upgrade

--- packages/tacoscript/bin/_tacoscript-node.js
+++ packages/tacoscript/bin/_tacoscript-node.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-require("../lib/cli/commands/_node")({}, process.argv.slice(2), function(code) {
+require("../lib/cli/commands/_node").default({}, process.argv.slice(2), function(code) {
   if (typeof code === "number" && code !== 0) process.exit(code);
 });
