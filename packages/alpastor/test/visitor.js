53c3b13e9d9dc8809c588fe5cf4936087a77f3fd
Jordan Klassen <jordan@klassen.me.uk>
2016-07-08 12:53:23 -0700

    test: move tests for sourceElements getters to where they moved

--- packages/alpastor/test/visitor.js
+++ packages/alpastor/test/visitor.js
@@ -9,22 +9,4 @@ const NodePath = require('comal-traverse').NodePath;
 const memberExpressionAst = require('./fixtures/visitor/between/member-expression.ast.json')
 
 suite('alpastor', function () {
-  test('Visitor#between', function () {
-    const child = memberExpressionAst.expression;
-    const left = NodePath.get({parent: child, container: child, key: 'object'})
-    const right = NodePath.get({parent: child, container: child, key: 'property'})
-    expect(visitor.between(left, right)).deep.equals([child.sourceElements[1]])
-  })
-
-  test('Visitor#before', function () {
-    const child = memberExpressionAst.expression;
-    const right = NodePath.get({parent: child, container: child, key: 'property'})
-    expect(visitor.before(right)).deep.equals([child.sourceElements[0], child.sourceElements[1]])
-  })
-
-  test('Visitor#after', function () {
-    const child = memberExpressionAst;
-    const left = NodePath.get({parent: child, container: child, key: 'expression'})
-    expect(visitor.after(left)).deep.equals([child.sourceElements[1]])
-  })
 })
---
+++ packages/comal-traverse/test/source-elements.js
@@ -0,0 +1,34 @@
+require('source-map-support').install()
+
+const expect = require('chai').expect
+
+const NodePath = require('../lib/index').NodePath;
+
+const memberExpressionAst = require('./fixtures/source-elements/member-expression.ast.json')
+
+suite('comal-traverse/source-elements', function () {
+  test('Visitor#between', function () {
+    const child = memberExpressionAst.expression;
+    const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
+    const parentPath = NodePath.get({parent: memberExpressionAst, container: memberExpressionAst, key: 'expression'}).setContext(ctx);
+    const left = NodePath.get({parentPath: parentPath, parent: child, container: child, key: 'object'}).setContext(ctx);
+    const right = NodePath.get({parentPath: parentPath, parent: child, container: child, key: 'property'}).setContext(ctx);
+    expect(left.srcElUntil(right)).deep.equals([child.sourceElements[1]])
+  })
+
+  test('Visitor#before', function () {
+    const child = memberExpressionAst.expression;
+    const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
+    const right = NodePath.get({parent: child, container: child, key: 'property'}).setContext(ctx);
+    expect(right.srcElBefore()).deep.equals([child.sourceElements[0], child.sourceElements[1]])
+  })
+
+  test('Visitor#after', function () {
+    const child = memberExpressionAst;
+    const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
+    const parentPath = new NodePath({}, null).setContext(ctx);
+    parentPath.node = child;
+    const left = NodePath.get({parent: child, parentPath: parentPath, container: child, key: 'expression'}).setContext(ctx);
+    expect(left.srcElAfter()).deep.equals([child.sourceElements[1]])
+  })
+})
