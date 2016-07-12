df1d9337e4632720bbc4d11c8fcdd1e5cf9d57dc
Jordan Klassen <jordan@klassen.me.uk>
2016-07-11 18:52:38 -0700

    test: fix test names

--- packages/comal-traverse/test/source-elements.js
+++ packages/comal-traverse/test/source-elements.js
@@ -7,7 +7,7 @@ const NodePath = require('../lib/index').NodePath;
 const memberExpressionAst = require('./fixtures/source-elements/member-expression.ast.json')
 
 suite('comal-traverse/source-elements', function () {
-  test('Visitor#between', function () {
+  test('NodePath#srcElUntil', function () {
     const child = memberExpressionAst.expression;
     const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
     const parentPath = NodePath.get({parent: memberExpressionAst, container: memberExpressionAst, key: 'expression'}).setContext(ctx);
@@ -16,14 +16,14 @@ suite('comal-traverse/source-elements', function () {
     expect(left.srcElUntil(right)).deep.equals([child.sourceElements[1]])
   })
 
-  test('Visitor#before', function () {
+  test('NodePath#srcElBefore', function () {
     const child = memberExpressionAst.expression;
     const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
     const right = NodePath.get({parent: child, container: child, key: 'property'}).setContext(ctx);
     expect(right.srcElBefore()).deep.equals([child.sourceElements[0], child.sourceElements[1]])
   })
 
-  test('Visitor#after', function () {
+  test('NodePath#srcElAfter', function () {
     const child = memberExpressionAst;
     const ctx = {opts: {sourceElementsSource: 'sourceElements'}};
     const parentPath = new NodePath({}, null).setContext(ctx);
@@ -31,4 +31,6 @@ suite('comal-traverse/source-elements', function () {
     const left = NodePath.get({parent: child, parentPath: parentPath, container: child, key: 'expression'}).setContext(ctx);
     expect(left.srcElAfter()).deep.equals([child.sourceElements[1]])
   })
+
+  test('NodePath#leadingWhitespace')
 })
